3e8d79b7ea215508bc0178c438eb7ca887032b96
Christer van der Meeren <cmeeren@protonmail.com>
2019-01-12 23:14:47 +0100

    Initial commit

---
+++ Fable.Import.Downshift/Bindings.fs
@@ -0,0 +1,243 @@
+module Fable.Import.Downshift
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fable.Import.React
+
+
+let resetIdCounter () : unit =
+  import "resetIdCounter" "downshift"
+
+
+type [<StringEnum>] [<RequireQualifiedAccess>] StateChangeTypes =
+  | [<CompiledName "__autocomplete_unknown__">] Unknown
+  | [<CompiledName "__autocomplete_mouseup__">] MouseUp
+  | [<CompiledName "__autocomplete_item_mouseenter__">] ItemMouseEnter
+  | [<CompiledName "__autocomplete_keydown_arrow_up__">] KeyDownArrowUp
+  | [<CompiledName "__autocomplete_keydown_arrow_down__">] KeyDownArrowDown
+  | [<CompiledName "__autocomplete_keydown_escape__">] KeyDownEscape
+  | [<CompiledName "__autocomplete_keydown_enter__">] KeyDownEnter
+  | [<CompiledName "__autocomplete_click_item__">] ClickItem
+  | [<CompiledName "__autocomplete_blur_input__">] BlurInput
+  | [<CompiledName "__autocomplete_change_input__">] ChangeInput
+  | [<CompiledName "__autocomplete_keydown_space_button__">] KeyDownSpaceButton
+  | [<CompiledName "__autocomplete_click_button__">] ClickButton
+  | [<CompiledName "__autocomplete_blur_button__">] BlurButton
+  | [<CompiledName "__autocomplete_controlled_prop_updated_selected_item__">] ControlledPropUpdatedSelectedItem
+  | [<CompiledName "__autocomplete_touchstart__">] TouchStart
+
+type DownshiftState<'Item> =
+  abstract highlightedIndex: int option with get, set
+  abstract inputValue: string with get, set
+  abstract isOpen: bool with get, set
+  abstract selectedItem: 'Item option with get, set
+
+type StateChanges<'Item> =
+  abstract ``type``: StateChangeTypes with get, set
+  inherit DownshiftState<'Item>
+
+type Actions<'Item> =
+  abstract clearItems: (unit -> unit) with get, set
+  abstract clearSelection: ((unit -> unit) -> unit) with get, set
+  abstract closeMenu: ((unit -> unit) -> unit) with get, set
+  abstract itemToString: ('Item option -> string) with get, set
+  abstract openMenu: ((unit -> unit) -> unit) with get, set
+  abstract reset: (obj -> (unit -> unit) -> unit) with get, set
+  abstract selectHighlightedItem: (obj -> (unit -> unit) -> unit) with get, set
+  abstract selectItem: ('Item -> obj -> (unit -> unit) -> unit) with get, set
+  abstract selectItemAtIndex: (int -> obj -> (unit -> unit) -> unit) with get, set
+  abstract setHighlightedIndex: (int -> obj -> (unit -> unit) -> unit) with get, set
+  abstract setItemCount: (int -> unit) with get, set
+  abstract setState: (obj -> unit) with get, set
+  abstract toggleMenu: (obj -> (unit -> unit) -> unit) with get, set
+  abstract unsetItemCount: (unit -> unit) with get, set
+
+type StateAndHelpers<'Item> =
+  inherit Actions<'Item>
+  inherit DownshiftState<'Item>
+  abstract getInputProps: (#seq<IHTMLProp> -> seq<IHTMLProp>) with get, set
+  abstract getItemProps: ('Item -> #seq<IHTMLProp> -> seq<IHTMLProp>) with get, set
+  abstract getLabelProps: (#seq<IHTMLProp> -> seq<IHTMLProp>) with get, set
+  abstract getMenuProps: (#seq<IHTMLProp> -> seq<IHTMLProp>) with get, set
+  abstract getRootProps: (#seq<IHTMLProp> -> seq<IHTMLProp>) with get, set
+  abstract getToggleButtonProps: (#seq<IHTMLProp> -> seq<IHTMLProp>) with get, set
+
+type A11yStatusMessageOptions<'Item> =
+  abstract highlightedIndex: int option with get, set
+  abstract highlightedItem: 'Item option with get, set
+  abstract inputValue: string with get, set
+  abstract isOpen: bool with get, set
+  abstract itemToString: ('Item option -> string) with get, set
+  abstract previousResultCount: int with get, set
+  abstract resultCount: int with get, set
+  abstract selectedItem: 'Item option with get, set
+
+type DownshiftProps<'Item> =
+  | Children of (StateAndHelpers<'Item> -> ReactElement)
+  | DefaultHighlightedIndex of int
+  | DefaultIsOpen of bool
+  | Environment of Browser.Window
+  | GetA11yStatusMessage of (A11yStatusMessageOptions<'Item> -> string)
+  | GetItemId of (int -> string)
+  | HighlightedIndex of int option
+  | Id of string
+  | InitialHighlightedIndex of int option
+  | InitialInputValue of string
+  | InitialIsOpen of bool
+  | InitialSelectedItem of U2<'Item option, 'Item list>
+  | InputId of string
+  | InputValue of string
+  | IsOpen of bool
+  | ItemCount of int
+  | ItemToString of ('Item option -> string)
+  | LabelId of string
+  | MenuId of string
+  | OnChange of ('Item -> StateAndHelpers<'Item> -> unit)
+  | OnInputValueChange of (string -> StateAndHelpers<'Item> -> unit)
+  | OnOuterClick of (StateAndHelpers<'Item> -> unit)
+  | OnSelect of ('Item -> StateAndHelpers<'Item> -> unit)
+  | OnStateChange of (StateChanges<'Item> -> StateAndHelpers<'Item> -> unit)
+  | OnUserAction of (StateChanges<'Item> -> StateAndHelpers<'Item> -> unit)
+  | ScrollIntoView of (Browser.HTMLElement -> Browser.HTMLElement -> unit)
+  | SelectedItem of 'Item option
+  | SelectedItemChanged of ('Item -> 'Item -> bool)
+  | StateReducer of (DownshiftState<'Item> -> StateChanges<'Item> -> DownshiftState<'Item>)
+  | SuppressRefError of bool
+  interface IHTMLProp
+
+type GetPropsCommonOptions =
+  | SuppressRefError of bool
+  interface IHTMLProp
+
+type GetInputPropsOptions =
+  | Disabled of bool
+  interface IHTMLProp
+
+type GetItemPropsOptions =
+  | Index of int
+  | IsSelected of bool
+  | Disabled of bool
+  interface IHTMLProp
+
+type GetMenuPropsOptions =
+  | RefKey of string
+  | [<CompiledName("aria-label")>] AriaLabel of string
+  interface IHTMLProp
+
+type GetRootPropsOptions =
+  | RefKey of string
+  interface IHTMLProp
+
+type GetToggleButtonPropsOptions =
+  | Disabled of bool
+  interface IHTMLProp
+
+
+module Internal =
+
+  type RawPropGetters<'Item> =
+    abstract getInputProps: (obj -> obj) with get, set
+    abstract getItemProps: (obj -> obj) with get, set
+    abstract getLabelProps: (obj -> obj) with get, set
+    abstract getMenuProps: (obj -> obj -> obj) with get, set
+    abstract getRootProps: (obj -> obj -> obj) with get, set
+    abstract getToggleButtonProps: (obj -> obj) with get, set
+
+  type RawStateAndHelpers<'Item> =
+    inherit RawPropGetters<'Item>
+    inherit Actions<'Item>
+    inherit DownshiftState<'Item>
+
+  type RawDownshiftProps<'Item> =
+    | Children of (RawStateAndHelpers<'Item> -> ReactElement)
+    | DefaultHighlightedIndex of int option
+    | DefaultIsOpen of bool
+    | Environment of Browser.Window
+    | GetA11yStatusMessage of (A11yStatusMessageOptions<'Item> -> string) option
+    | GetItemId of (int -> string)
+    | HighlightedIndex of int option
+    | Id of string
+    | InitialHighlightedIndex of int option
+    | InitialInputValue of string
+    | InitialIsOpen of bool
+    | InitialSelectedItem of U2<'Item option, 'Item list>
+    | InputId of string
+    | InputValue of string
+    | IsOpen of bool
+    | ItemCount of int
+    | ItemToString of ('Item option -> string)
+    | LabelId of string
+    | MenuId of string
+    | OnChange of ('Item -> RawStateAndHelpers<'Item> -> unit)
+    | OnInputValueChange of (string -> RawStateAndHelpers<'Item> -> unit)
+    | OnOuterClick of (RawStateAndHelpers<'Item> -> unit)
+    | OnSelect of ('Item -> RawStateAndHelpers<'Item> -> unit)
+    | OnStateChange of (StateChanges<'Item> -> RawStateAndHelpers<'Item> -> unit)
+    | OnUserAction of (StateChanges<'Item> -> RawStateAndHelpers<'Item> -> unit)
+    | ScrollIntoView of (Browser.HTMLElement -> Browser.HTMLElement -> unit)
+    | SelectedItem of 'Item option
+    | SelectedItemChanged of ('Item -> 'Item -> bool) option
+    | StateReducer of (DownshiftState<'Item> -> StateChanges<'Item> -> DownshiftState<'Item>) option
+    | SuppressRefError of bool option
+    interface IHTMLProp
+
+  [<Emit("
+  var keys = [];
+  for(var key in $0){ keys.push([key, $0[key]]); }
+  return keys;
+  ")>]
+  let toKeyValueList (o: obj) : (string * obj) list = jsNative
+
+  let stateAndHelpersOfRaw (raw: RawStateAndHelpers<'Item>) : StateAndHelpers<'Item> =
+
+    // Helper to convert the props to an obj, pass to rawFun, and convert back to seq<IHTMLProp>.
+    let rawProps (rawFun: obj -> obj) : seq<IHTMLProp> -> seq<IHTMLProp> =
+      keyValueList CaseRules.LowerFirst
+      >> rawFun
+      >> toKeyValueList
+      >> Seq.map (fun kv -> kv |> HTMLAttr.Custom :> IHTMLProp)
+
+    // Same as above, but used for functions accepting a second object
+    // with suppressRefError = true.
+    let rawPropsExtra (rawFun: obj -> obj -> obj) (props: seq<IHTMLProp>) : seq<IHTMLProp> =
+      let hasSuppressRefError =
+        props
+        |> Seq.choose (function :? GetPropsCommonOptions as x -> Some x | _ -> None)
+        |> Seq.exists (function GetPropsCommonOptions.SuppressRefError true -> true | _ -> false)
+      let filteredProps =
+        props
+        |> Seq.filter (function :? GetPropsCommonOptions -> false | _ -> true)
+        |> keyValueList CaseRules.LowerFirst
+      let extraProps = createEmpty<obj>
+      if hasSuppressRefError then extraProps?suppressRefError <- true
+      rawFun filteredProps extraProps
+      |> toKeyValueList
+      |> Seq.map (fun kv -> kv |> HTMLAttr.Custom :> IHTMLProp)
+
+    let s = JS.Object.assign(createEmpty<_>, raw) :?> StateAndHelpers<'Item>
+    s.getInputProps <- (fun props -> props |> rawProps raw.getInputProps)
+    s.getItemProps <- (fun item props -> props |> Seq.append [HTMLAttr.Custom ("item", item)] |> rawProps raw.getItemProps)
+    s.getLabelProps <- (fun props -> props |> rawProps raw.getLabelProps)
+    s.getMenuProps <- (fun props -> props |> rawPropsExtra raw.getMenuProps)
+    s.getRootProps <- (fun props -> props |> rawPropsExtra raw.getRootProps)
+    s.getToggleButtonProps <- (fun props -> props |> rawProps raw.getToggleButtonProps)
+    s
+
+  let downshiftPropToRaw (prop: DownshiftProps<'Item>) : RawDownshiftProps<'Item> =
+    match prop with
+    | DownshiftProps.Children f -> RawDownshiftProps.Children (stateAndHelpersOfRaw >> f)
+    | DownshiftProps.OnChange f -> RawDownshiftProps.OnChange (fun i s -> f i (stateAndHelpersOfRaw s))
+    | DownshiftProps.OnInputValueChange f -> RawDownshiftProps.OnInputValueChange (fun str s -> f str (stateAndHelpersOfRaw s))
+    | DownshiftProps.OnOuterClick f -> RawDownshiftProps.OnOuterClick (stateAndHelpersOfRaw >> f)
+    | DownshiftProps.OnSelect f -> RawDownshiftProps.OnSelect (fun i s -> f i (stateAndHelpersOfRaw s))
+    | DownshiftProps.OnStateChange f -> RawDownshiftProps.OnStateChange (fun o s -> f o (stateAndHelpersOfRaw s))
+    | DownshiftProps.OnUserAction f -> RawDownshiftProps.OnUserAction (fun o s -> f o (stateAndHelpersOfRaw s))
+    | p -> !!p
+
+
+let inline downshift<'Item> (b : seq<DownshiftProps<'Item>>) (c: StateAndHelpers<'Item> -> ReactElement) : ReactElement =
+  let b = Seq.append b [DownshiftProps.Children c]
+  let b = b |> Seq.map (fun d -> d |> Internal.downshiftPropToRaw)
+  ofImport "default" "downshift" (keyValueList CaseRules.LowerFirst b) []
---
+++ README.md
@@ -0,0 +1,93 @@
+# Fable.Import.Downshift
+
+This package provides Fable bindings for [downshift](https://github.com/paypal/downshift).
+
+## Installation
+
+* Install the `downshift` npm package:
+  * using npm: `npm install downshift`
+  * using yarn: `yarn add downshift`
+
+* Install the bindings:
+  * using dotnet: `dotnet add package Fable.Import.Downshift`
+  * using paket: `paket add Fable.Import.Downshift`
+
+## Usage
+
+Familiarity with [downshift’s native JS API ](https://github.com/paypal/downshift) and how downshift works is assumed. Fable.Import.Downshift simply provide a Fable-React idiomatic API with most props as DU cases.
+
+As you might expect from downshift’s native API, the `downshift` helper component does not accept a list of children, but a function that receives the downshift state/helpers and returns a React component.
+
+The downshift prop getters are functions `#seq<IHTMLProp> -> seq<IHTMLProp>` where the input props are passed to the respective prop getter. (The only exception is `getItemProps`, which also takes `'Item` as its first parameter since it’s a required prop.)
+
+Below is the [downshift readme example](https://github.com/paypal/downshift/blob/39ca5d449e25dfe89b8cbd58b1c6f5a8d3e04329/README.md#usage) converted to F#:
+
+```f#
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fable.Import
+open Fable.Import.Downshift
+
+type Item = { Value: string }
+
+let items = 
+  [ { Value = "apple" }
+    { Value = "pear" }
+    { Value = "orange" }
+    { Value = "grape" }
+    { Value = "banana" } ]
+
+let view =
+  downshift [
+    OnChange (fun item _ -> Browser.window.alert(sprintf "You selected %s" item.Value))
+    ItemToString (function Some item -> item.Value | None -> "")
+  ] (fun ds ->
+    div [] [
+      label (ds.getLabelProps []) [ str "Enter a fruit" ]
+      input (ds.getInputProps [])
+      ul (ds.getMenuProps []) [
+        if ds.isOpen then
+          yield
+            items
+            |> List.filter (fun item -> item.Value.Contains ds.inputValue)
+            |> List.mapi (fun index item ->
+                li (ds.getItemProps item [
+                  Key item.Value
+                  Index index
+                  Style [
+                    BackgroundColor 
+                      (if ds.highlightedIndex = Some index then "lightgray" else "white")
+                    FontWeight (if ds.selectedItem = Some item then "bold" else "normal")
+                  ]
+                ]) [
+                  str item.Value
+                ]
+            )
+            |> ofList
+      ]
+    ]
+  )
+
+```
+
+### Props specific to downshift
+
+* The DU type `DownshiftProps` contains props you can pass to the `downshift` helper.
+* The DU types `GetInputPropsOptions`, `GetItemPropsOptions`, etc. contains props you can pass to the respective prop getters.
+  * Note that `GetItemPropsOptions` does not contain an `Item` case; as mentioned previously, since this is a required prop, it’s passed as the first argument to `getItemProps`.
+* The special `SuppressRefError` prop is available in the DU type `GetPropsCommonOptions`. You pass this as a normal prop alongside others, and the binding internals takes care of passing that prop as a separate object as required by the downshift API.
+
+Changelog
+---------
+
+#### 1.0.0 (2019-01-12)
+
+* Initial release
+
+## Deployment checklist
+
+1. Make necessary changes to the code
+2. Update the changelog
+3. Update the version and release notes in the package info, as well as the message stating which `downshift` version the bindings are created for.
+4. Commit and tag the commit (this is what triggers deployment from  AppVeyor). For consistency, the tag should ideally be in the format `v1.2.3`.
+5. Push the changes and the tag to the repo. If AppVeyor build succeeds, the package is automatically published to NuGet.
